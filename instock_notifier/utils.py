9bd1a4060f0d85c44b9f60f175796f343a7fa6e9
fpatseas <fpatseas@realize.gr>
2020-11-12 10:17:06 +0200

    Include send_email in utils

--- instock_notifier/utils.py
+++ instock_notifier/utils.py
@@ -1,4 +1,28 @@
+import config
+
+from mailjet_rest import Client
 from datetime import date, datetime
+
+def send_email(sender, recipients, subject, body):
+    try:
+        api_key = config.SMTP_API_KEY
+        api_secret = config.SMTP_API_SECRET
+        mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+        data = {
+            'Messages':
+            [
+                {
+	                "From": sender,
+	                "To": recipients,
+	                "Subject": subject,
+	                "HTMLPart": body,
+	                "CustomID": "AppGettingStartedTest"
+                }
+            ]
+        }
+        mailjet.send.create(data=data)
+    except Exception as e:
+        print(e)
 
 def json_serial(obj):
 	if isinstance(obj, (datetime, date)):
